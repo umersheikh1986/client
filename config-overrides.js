026db312226c30cedcdf5f9417da09163e4c989b
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2025-09-11 04:44:31 +0500

    ss

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,27 @@
-const { override, addWebpackResolve, addWebpackPlugin } = require('customize-cra');
-const webpack = require('webpack');
+const {
+  override,
+  addWebpackResolve,
+  addWebpackPlugin,
+} = require("customize-cra");
+const webpack = require("webpack");
 
 module.exports = override(
   addWebpackResolve({
     fallback: {
-      "crypto": require.resolve("crypto-browserify"),
-      "assert": require.resolve("assert/"),
-      "stream": require.resolve("stream-browserify"),
-      "vm": require.resolve("vm-browserify"),
-      "buffer": require.resolve("buffer"),
-      "util": require.resolve("util/"),
-      "path": require.resolve("path-browserify"),
-      "os": require.resolve("os-browserify/browser")
-    }
+      // "crypto": require.resolve("crypto-browserify"),
+      assert: require.resolve("assert/"),
+      stream: require.resolve("stream-browserify"),
+      vm: require.resolve("vm-browserify"),
+      buffer: require.resolve("buffer"),
+      util: require.resolve("util/"),
+      path: require.resolve("path-browserify"),
+      os: require.resolve("os-browserify/browser"),
+    },
   }),
-  addWebpackPlugin(new webpack.ProvidePlugin({
-    process: 'process/browser',
-    Buffer: ['buffer', 'Buffer']
-  }))
+  addWebpackPlugin(
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    })
+  )
 );
